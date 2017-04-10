bd893fd4cd7bc104bf14b925c9a5943ae1570314
Your Name <arssonist@yahoo.com>
2017-04-10 13:18:05 -0400

    generator working with one api, and has basic stylings

--- quote.js
+++ quote.js
@@ -1,8 +1,15 @@
 $(function() {
   $('#quote-button').on('click',(e) => {
   // var url = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=";
-  var url = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en"
-  // var url = "https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en"
+//url for AJAX- NO JSONP
+  // var url = "http://api.forismatic.com/api/1.0/method=getQuote&key=457653&format=json&lang=en"
+// url for getJSON
+  var url = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?"
+    // $.ajax({
+    //   url:url,
+    //   method:'GET',
+    //   dataType:'JSONP'
+    // }).done(function(json){
     $.getJSON(url,function(json){
       var author = json.quoteAuthor
       var text   = json.quoteText
@@ -13,7 +20,8 @@ $(function() {
         // Object.keys(json).forEach((key) => {
         // $("#quote-putter").append(key.quoteText)
         // })
-        $(".quotes").empty()
+        $("#quote-text-box").empty()
+        $("#quote-author-box").empty()
         $("#quote-author-box").append(author)
         $("#quote-text-box").append(text)
       })
