d32a8c2521e2cd1e4e022a145a35c518b7abcaae
Your Name <arssonist@yahoo.com>
2017-04-10 23:04:44 -0400

    http working in code pen;moved from code project;twitter tweeter not working yet;

--- quote.js
+++ quote.js
@@ -1,10 +1,26 @@
 $(function() {
+
+  console.log(getQuote())
+  $('.twitter-share-button').attr('href', (index, value) => { //value is twitter.com
+    return value + "/intent/tweet?text=" + getQuote()
+  })
+
   $('#quote-button').on('click',(e) => {
+
   // var url = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=";
 //url for AJAX- NO JSONP
   // var url = "http://api.forismatic.com/api/1.0/method=getQuote&key=457653&format=json&lang=en"
 // url for getJSON
+
+// function to open twitter window
+// window.open()
+//
+// function to twitter
+// call open window mesageas params
+// inside callback get text, and send it bak out here
+
   var url = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?"
+
     // $.ajax({
     //   url:url,
     //   method:'GET',
@@ -13,8 +29,8 @@ $(function() {
     $.getJSON(url,function(json){
       var author = json.quoteAuthor
       var text   = json.quoteText
-      console.log(author)
-      console.log(text)
+      // console.log(author)
+      // console.log(text)
       // var string = JSON.stringify(json)
       // string = string.slice(0)
         // Object.keys(json).forEach((key) => {
@@ -24,6 +40,14 @@ $(function() {
         $("#quote-author-box").empty()
         $("#quote-author-box").append(author)
         $("#quote-text-box").append(text)
+        // $(".twitter-share-button").attr('href', twitter_url)
       })
     })
   })
+
+var getQuote = function(){
+  var text = $('#quote-text-box').html()
+  if (text != "Quotes goe here!"){
+    return text
+  }
+}
