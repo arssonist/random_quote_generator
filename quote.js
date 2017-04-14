60a83ccfc939d01c98b30eda1d285d694939eb41
Your Name <arssonist@yahoo.com>
2017-04-13 20:34:26 -0400

    added dif tweet button; worked on getting author pulled in; removed widget from script

--- quote.js
+++ quote.js
@@ -1,20 +1,36 @@
 $(function() {
 
-
   var url = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?"
-
+  var value = "https://twitter.com";
 
 //on load, check if box is empty. If it is, then load quote
-  if($("#quote-text-box").html() === ""){
-    $.getJSON(url,function(json){
-      var author = json.quoteAuthor
-      var text   = json.quoteText
-
-        $("#quote-author-box").append(author)
-        $("#quote-text-box").append(text)
-      })
-    }
-
+//   if($("#quote-text-box").html() === ""){
+//
+//     $.getJSON(url,function(json){
+//       var author = json.quoteAuthor
+//       var text   = json.quoteText
+// //append quote to page
+//         $("#quote-author-box").append(author)
+//         $("#quote-text-box").append(text)
+//
+// //get quote content
+//           let text_content = $('#quote-text-box').html()
+// //get author content
+//           let author_content = $('#quote-author-box').html();
+//           console.log(author_content)
+//           let full_quote = `${text_content} -${author_content}`
+//
+//           let make_string = value + "/intent/tweet?text=" + full_quote
+//           console.log(make_string)
+//  //add click to change the a href string
+//     $(".twitter-share-button").click(function(e){
+//     // e.preventDefault()
+//      $('.twitter-share-button').attr('href',make_string)
+//           console.log(make_string)
+//        })
+//      })
+//
+//   }
 
 //on click, give quote
   $('#quote-button').on('click',(e) => {
@@ -45,24 +61,39 @@ $(function() {
         $("#quote-text-box").append(text)
         // $(".twitter-share-button").attr('href', twitter_url)
       })
-    })
+
+
+//
+//
+//
+//   })
+//
+//
+//
+    $('.twitter-share-button').click(function(e){
+      let value = "https://twitter.com";
+      let text_content = $('#quote-text-box').html()
+//get author content
+    let author_content = $('#quote-author-box').html();
+      console.log(author_content)
+      let full_quote = `${text_content} -${author_content}`
+
+      let make_string = value + "/intent/tweet?text=" + full_quote
+      console.log(make_string)
+//add click to change the a href string
+  $("twitter-share-button").click(function(e){
+    // e.preventDefault()
+    $('.twitter-share-button').attr('href',make_string)
+    console.log(make_string)
+  })
   })
 
 
-
-$('.twitter-share-button').click(function(e){
-  let value = "https://twitter.com";
-  let text = $('#quote-text-box').html()
-  console.log(text)
-  let make_string = value + "/intent/tweet?text=" + text
-  console.log(make_string)
-  $('.twitter-share-button').attr('href',make_string)
-  console.log(make_string)
-    })
-
-var getQuote = function(){
-  var text = $('#quote-text-box').html()
-  if (text != ""){
-    return text
-  }
-}
+// var getQuote = function(){
+//   var text = $('#quote-text-box').html()
+//   if (text != ""){
+//     return text
+//   }
+// }
+})
+})
